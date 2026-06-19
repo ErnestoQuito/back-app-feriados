b46c09396d3068cc95676cd90ae53d8271955e6a
Ernesto <ernesto.quito@externos-cl.cencosud.com>
2026-06-19 16:34:48 -0500

    Se cborra archivos innecesarios y se modifica archivo de configuracion

--- app/core/database.py
+++ app/core/database.py
@@ -1,26 +1,16 @@
-from os import environ
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+from .config import settings
 
-DATABASE_URL = environ.get("DATABASE_URL", None)
-
-if not DATABASE_URL:
-    raise Exception(
-        "..:: the database variable not found. Please, check your configuration."
-    )
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 base = declarative_base()
 
 
 def init_db():
     """Importación local (Lazy Import) de todos los modelos del proyecto"""
-    from app.modules.country.models import CountryModel
+    from app.modules.country.models import CountryModel  # noqa
 
     # Crea las tablas si o no existe (ideal para desarrollo inicial)
     base.metadata.create_all(bind=engine)
